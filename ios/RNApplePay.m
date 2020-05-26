d5fd6961eccb2cc71f25a4ecf7b9043edac9380a
agungjk <hiagungjk@gmail.com>
2020-05-26 15:04:10 +0700

    modify callback response

--- ios/RNApplePay.m
+++ ios/RNApplePay.m
@@ -125,8 +125,39 @@ RCT_EXPORT_METHOD(complete:(NSNumber *_Nonnull)status promiseWithResolver:(RCTPr
 {
     self.completion = completion;
     if (self.requestPaymentResolve != NULL) {
-        NSString *paymentData = [[NSString alloc] initWithData:payment.token.paymentData encoding:NSUTF8StringEncoding];
-        self.requestPaymentResolve(paymentData);
+        NSString *paymentDataString = [[NSString alloc] initWithData:payment.token.paymentData encoding:NSUTF8StringEncoding];
+        NSDictionary *paymentDataDictionary = [NSJSONSerialization JSONObjectWithData:payment.token.paymentData options:NSJSONReadingMutableContainers error:nil];
+        
+        NSString *paymentType = @"";
+        switch (payment.token.paymentMethod.type) {
+            case PKPaymentMethodTypeDebit:
+                paymentType = @"debit";
+                break;
+            case PKPaymentMethodTypeCredit:
+                paymentType = @"credit";
+                break;
+            case PKPaymentMethodTypeStore:
+                paymentType = @"store";
+                break;
+            case PKPaymentMethodTypePrepaid:
+                paymentType = @"prepaid";
+                break;
+            default:
+                paymentType = @"unknown";
+                break;
+        }
+        
+        NSDictionary *paymentMethodDictionary = @{@"network": payment.token.paymentMethod.network, @"type": paymentType, @"displayName": payment.token.paymentMethod.displayName};
+        NSDictionary *cryptogramDictionary = @{@"paymentData": paymentDataDictionary,
+                                                @"transactionIdentifier": payment.token.transactionIdentifier,
+                                                @"paymentMethod": paymentMethodDictionary
+                                                };
+
+        NSDictionary *cardCryptogramPacketDictionary = cryptogramDictionary;
+        NSData *cardCryptogramPacketData = [NSJSONSerialization dataWithJSONObject:cardCryptogramPacketDictionary options:0 error:nil];
+        NSString *cardCryptogramPacketString = [[NSString alloc] initWithData:cardCryptogramPacketData encoding:NSUTF8StringEncoding];
+
+        self.requestPaymentResolve(cardCryptogramPacketString);
         self.requestPaymentResolve = NULL;
     }
 }
